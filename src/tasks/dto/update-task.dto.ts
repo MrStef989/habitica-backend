19f386494104389e883a268f6247688104248401
MrStef989 <borodovgerman123@gmail.com>
2026-04-08 15:00:59 +0300

    Update update-task.dto.ts

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,8 +1,37 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTaskDto } from './create-task.dto';
-import { IsBoolean, IsOptional } from 'class-validator';
+import {
+  IsString, IsEnum, IsOptional, IsBoolean,
+  IsDateString, MaxLength, MinLength,
+} from 'class-validator';
+import { TaskDifficulty } from '../task.entity';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+export class UpdateTaskDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(1)
+  @MaxLength(255)
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000)
+  notes?: string;
+
+  @IsOptional()
+  @IsEnum(TaskDifficulty)
+  difficulty?: TaskDifficulty;
+
+  @IsOptional()
+  @IsDateString()
+  dueDate?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  habitPositive?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  habitNegative?: boolean;
+
   @IsOptional()
   @IsBoolean()
   isActive?: boolean;
