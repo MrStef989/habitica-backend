b1f6d1f88d2f632bb447f738576edccaff83ce5c
MrStef989 <borodovgerman123@gmail.com>
2026-04-08 15:35:56 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
-
+import { SeedModule } from './database/seed.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
